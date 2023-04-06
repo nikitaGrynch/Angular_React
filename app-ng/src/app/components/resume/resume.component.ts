f113ed74b737716e9d87a0d46e2b79a71b13be3b
grynch <nikita.grynch@gmail.com>
2023-04-07 01:58:26 +0300

    binding, pipe, ngClass, ngStyle HW

--- app-ng/src/app/components/resume/resume.component.ts
+++ app-ng/src/app/components/resume/resume.component.ts
@@ -3,38 +3,52 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
-  styleUrls: ['./resume.component.scss']
+  styleUrls: ['./resume.component.scss'],
 })
 export class ResumeComponent {
-  fullName: String = "Grynchenko Nikita";
-  email: String = "nikita.grynch@gmail.com";
-  phoneNumber: String = "+3801231122";
-  city: String = "Odessa";
-  country: String = "Ukraine";
-  skills: String[] = [
-    ".NET",
-    "JS",
-    "Python",
-    "SQL"
-  ]
+  fullName: string = 'Grynchenko Nikita';
+  nameColor: string = 'white';
+  nameFontSize: string = '2em';
+  email: string = 'Nikita.grynch@gmail.com';
+  phoneNumber: string = '+3801231122';
+  city: string = 'Odessa';
+  country: string = 'Ukraine';
+  skills: any[] = [
+    {
+      name: '.NET',
+      isMain: true,
+    },
+    {
+      name: 'JS',
+      isMain: true,
+    },
+    {
+      name: 'Python',
+      isMain: false,
+    },
+    {
+      name: 'SQL',
+      isMain: false,
+    },
+  ];
   workExperience: any[] = [
     {
-      title: "Firefly Web Project",
-      year: 2022
+      title: 'Firefly web project',
+      year: 2022,
     },
     {
-      title: "Scholarships IOS App",
-      year: 2023
-    }
-  ]
+      title: 'Scholarships IOS app',
+      year: 2023,
+    },
+  ];
   links: any[] = [
     {
-      title: "GitHub",
-      url: "https://github.com/nikitaGrynch"
+      title: 'GitHub',
+      url: 'https://github.com/nikitaGrynch',
     },
     {
-      title: "LinkedIn",
-      url: "https://linkedin.com/in/nikita-grynchenko-0491ab232"
-    }
-  ]
+      title: 'LinkedIn',
+      url: 'https://linkedin.com/in/nikita-grynchenko-0491ab232',
+    },
+  ];
 }
